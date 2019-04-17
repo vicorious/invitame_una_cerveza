5623cd2bf91614885102ebcdf39cce91c15b5522
Alejandro Lindarte Castro <alejo.lindarte@outlook.com>
2019-04-17 11:14:12 -0500

    V0.3

--- template_html/reclamar_codigo.js
+++ template_html/reclamar_codigo.js
@@ -1,37 +1,78 @@
-$(document).ready(function() {
-	$("#scan").on('click', function() {
+$(document).ready(function() 
+{				
+	$("#scan").on('click', function() 
+	{
 		$("code").html('Scaneado...');
-		$('#qr').html5_qrcode(function(data){
-		         // do something when code is read
-		         $(".feedback").html('<img src="yes.png" style="height: 20px"> Codigo scaneado como: <strong>' +data +'</strong>');
-		    },
-		    function(error){
-		        //show read errors 
-		        $(".feedback").html('<img src="no.png" style="height: 20px"> No es posible escanear el codigo! Error: ' +error)
-		    }, function(videoError){
-		        //the video stream could be opened
-		        $(".feedback").html('<img src="no.png" style="height: 20px"> Error de video');
-		    }
+		$('#qr').html5_qrcode(function(data)
+		{
+			$(".feedback").css('display','block');
+		    // do something when code is read
+		    $(".feedback").html('<img src="yes.png" style="height: 20px"></img> Codigo scaneado como: <strong>' +data +'</strong>');
+			var canvas = document.getElementById('canvas');			
+			var context = canvas.getContext('2d');
+			var video = document.querySelector('video');
+			console.log(video);
+			context.drawImage(video, 0, 0, 600, 450);			
+			$("#qr").html5_qrcode_stop();
+			$("#divCanvas").css('display', 'block');
+			$("#txtFinal").html('<img src="yes.png" style="height: 20px"></img> Codigo scaneado como: <strong>' +data +'</strong>');
+			$("code").html('Click "Empezar Scaneo" para <b>Empezar a scanear el codigo QR');
+			$(".feedback").css('display','none');
+			$("#scan").removeClass('disabled');
+			$("#scan").removeAttr('disabled');
+			$("#stop").addClass('disabled');
+			$("#stop").attr('disabled','disabled');
+			$("#change").addClass('disabled');
+			$("#change").attr('disabled','disabled');
+			$("#codeAccept").css('display', 'block');
+			return;
+		},
+		function(error)
+		{
+			//show read errors 
+			$(".feedback").html('<img src="no.png" style="height: 20px"></img> No es posible escanear el codigo! Error: ' +error)
+		},
+		function(videoError)
+		{
+			//the video stream could be opened
+			$(".feedback").html('<img src="no.png" style="height: 20px"></img> Error de video: '+videoError);
+		}
 		);
 
 		$("#scan").addClass('disabled');
+		$("#scan").attr('disabled','disabled');
 		$("#stop").removeClass('disabled');
+		$("#stop").removeAttr('disabled');
 		$("#change").removeClass('disabled');
+		$("#change").removeAttr('disabled');
 	});
 
-	$("#stop").on('click', function() {
+	$("#stop").on('click', function() 
+	{
+		$("#divCanvas").css('display', 'none');
+		$("#codeAccept").css('display', 'none');
+		$("#txtFinal").html('');
 		$("#qr").html5_qrcode_stop();
 		$("code").html('Click "Empezar Scaneo" para <b>Empezar a scanear el codigo QR');
 
 		$("#scan").removeClass('disabled');
+		$("#scan").removeAttr('disabled');
 		$("#stop").addClass('disabled');
+		$("#stop").attr('disabled','disabled');
 		$("#change").addClass('disabled');
+		$("#change").attr('disabled','disabled');
 		$(".feedback").html("");
 	});
-	$("#change").on('click', function() {
+	$("#change").on('click', function() 
+	{
+		$("#divCanvas").css('display', 'block');
+		$("#codeAccept").css('display', 'none');
+		$("#txtFinal").html('');		
 		$("#qr").html5_qrcode_changeCamera();
 
 		$("#scan").addClass('disabled');
+		$("#scan").attr('disabled','disabled');
 		$("#stop").removeClass('disabled');
+		$("#stop").removeAttr('disabled');
 	});
 });
