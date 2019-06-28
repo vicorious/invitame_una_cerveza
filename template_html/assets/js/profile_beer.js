06c9a099145427f65c640ac880ded5e2824ee9e0
alejandro.lindarte.contractor <alejandro.lindarte.contractor@bbva.com>
2019-06-28 16:46:50 -0500

    Beer fixed

--- template_html/assets/js/profile_beer.js
+++ template_html/assets/js/profile_beer.js
@@ -3,8 +3,10 @@ document.addEventListener("DOMContentLoaded", function(event)
 	var slider = document.getElementById("myRange");
 	var output = document.getElementById("demo");
 	var output2 = document.getElementById("demo2");
+	let value_input = 52000;
 	output.innerHTML = slider.value;
-	output2.innerHTML = "52000";
+	output2.innerHTML = value_input;
+	let before_value = 0;
 	slider.oninput = function() 
 	{
 		output.innerHTML = this.value;
