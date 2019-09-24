47cffb7338c2a0040a1da780d5c849fff20fcf66
Alejandro Lindarte Castro <alindartec@psl.com.co>
2019-09-24 16:48:42 -0500

    Behavior profile beer html page

--- template_html/assets/js/profile_beer.js
+++ template_html/assets/js/profile_beer.js
@@ -30,4 +30,60 @@ document.addEventListener("DOMContentLoaded", function(event)
 		}
 	};
 	
+	var buttonAdd = document.getElementById("btnSumProduct");
+	
+	buttonAdd.addEventListener('click', function (event) 
+	{	
+		event.preventDefault();		
+		
+		var h2total = document.getElementById("H2total");				
+		var olProductos = document.getElementById("olProductos");
+		var cantidad = document.getElementById("slCantidad");
+		var presentacion = document.getElementById("slPresentacion");	
+		var defaultP = document.getElementById("pDefault");
+		if(defaultP != null || defaultP != undefined)
+		{
+			defaultP.remove();
+		}
+		
+		console.log(olProductos.childElementCount)
+
+		for(var i = 0; i < parseInt(cantidad.value); i++) 
+		{
+			var total = 0;
+			var il = document.createElement("li");
+			var totalNumerico = presentacion.value 
+			il.setAttribute("precio", totalNumerico);
+			total += parseInt(totalNumerico)
+			var buttonErase = document.createElement("button");
+			buttonErase.className += "fa fa-trash eraseElement";
+			buttonErase.setAttribute("aria-hidden", true);
+			buttonErase.addEventListener('click', function () 
+			{			
+				console.log("Borrado")			
+				console.log(olProductos.childElementCount)
+				if(olProductos.childElementCount == 1)
+				{
+					var defaultPN = document.createElement("p");
+					defaultPN.setAttribute("id", "pDefault");
+					defaultPN.innerHTML = "No records found...";
+					defaultPN.className += "letra negrita";
+					olProductos.appendChild(defaultPN);
+				}
+	
+				this.parentNode.remove();
+				h2total.innerHTML = (parseInt(h2total.innerHTML) - total)
+			});
+			il.className += "letra negrita liprecio";			
+
+			il.textContent += presentacion.options[presentacion.selectedIndex].text + " $ " + totalNumerico
+			il.appendChild(buttonErase);
+			olProductos.appendChild(il);		
+			//parseInt(presentacion.value).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');	
+			console.log("Cosa tener es: ");
+			h2total.innerHTML =  (parseInt(h2total.innerHTML) + total)
+		}			
+
+	}, false);	
+	
 });
