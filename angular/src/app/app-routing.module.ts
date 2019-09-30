c684851278905596e0a05e8a663c4acc2d787515
vicorious <alejo.lindarte@outlook.com>
2019-09-30 09:57:26 -0500

    Reclama codigo component

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AppComponent } from './app.component';
 import { LoginComponentComponent } from './login-component/login-component.component';
 import { BeerComponentComponent } from './beer-component/beer-component.component';
 import { WeatherComponentComponent } from './weather-component/weather-component.component';
@@ -8,6 +9,8 @@ import { BarComponentComponent } from './bar-component/bar-component.component';
 import { FooterComponentComponent } from './footer-component/footer-component.component';
 import { BeersPluralComponentComponent } from './beers-plural-component/beers-plural-component.component';
 import { ProfileComponentComponent } from './profile-component/profile-component.component';
+import { CodigoComponentComponent } from './codigo-component/codigo-component.component';
+import { ReclamaCodigoComponentComponent } from './reclama-codigo-component/reclama-codigo-component.component';
 
 
 const routes: Routes = 
@@ -19,6 +22,8 @@ const routes: Routes =
   { path: 'footer', component: FooterComponentComponent },
   { path: 'beers', component: BeersPluralComponentComponent },
   { path: 'profile', component: ProfileComponentComponent },
+  { path: 'codigo', component: CodigoComponentComponent },
+  { path: 'reclama', component: ReclamaCodigoComponentComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
 
