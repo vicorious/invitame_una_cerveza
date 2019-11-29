47e305c371b65897d3e6fa65d052ddffb39a03ae
Alejandro Lindarte Castro <alindartec@psl.com.co>
2019-11-29 10:50:43 -0500

    Jenkis test2

--- Python/facade_climate.py
+++ Python/facade_climate.py
@@ -10,3 +10,5 @@ class Climate:
         response = requests.get(Constant.weather_end_point,params={'q': city, 'units': Constant.weather_units, 'appid' : Constant.weather_key})
         json_response = response.json()
         climate_dto = ClimateDTO(weather = json_response["weather"][0], main = json_response["main"], wind = json_response["wind"], city = json_response["name"])
+        return climate_dto
+    
