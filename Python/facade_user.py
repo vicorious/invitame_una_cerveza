cfd8f12d37fa6549030d9059899db9ca93571c49
Alejandro Lindarte Castro <alindartec@psl.com.co>
2019-12-02 16:16:19 -0500

    DDL

--- Python/facade_user.py
+++ Python/facade_user.py
@@ -50,8 +50,8 @@ class UserFacade:
         try:
             _json_entrada = json.loads(_json)
             results = self.beer_connection.session.query(User).filter(
-                    User.name == _json_entrada["name"],
-                    User.password_token == _json_entrada["password_token"]).one()
+                User.name == _json_entrada["name"],
+                User.password_token == _json_entrada["password_token"]).one()
             self.beer_connection.session.close()
         except MultipleResultsFound as multiples_results:
             logging.debug('Multiple rows. Failed Integrity from database %s', multiples_results)
@@ -90,9 +90,9 @@ class UserFacade:
             _json_entrada = json.loads(_json)
             self.beer_connection.session.execute(
                 update(User, values={
-                       User.password_token: _json_entrada["new_password_token"]})).filter(
-                       User.name == _json_entrada["name"],
-                       User.password_token == _json_entrada["password_token"])
+                    User.password_token: _json_entrada["new_password_token"]})).filter(
+                           User.name == _json_entrada["name"],
+                           User.password_token == _json_entrada["password_token"])
             self.beer_connection.session.commit()
             self.beer_connection.session.close()
         except Exception as _excep:
