2ecdf9b26702d04c8ca6b6122e0d8a60cf5df58d
Alejandro Lindarte Castro <alindartec@psl.com.co>
2019-12-02 16:13:03 -0500

    DDL

--- Python/constant.py
+++ Python/constant.py
@@ -10,7 +10,7 @@ class Constant:
     port_default = '5432'
     db_default = 'invitame_una_cerveza'
     password_default = '*postgre123*'
-    postgre_exe_file = '\exe\postgresql-12.1-1-windows-x64.exe'
+    postgre_exe_file = '/exe/postgresql-12.1-1-windows-x64.exe'
     user = 'Alejo'
     weather_key = '8cde928e3f9fdc6ce35a4f5d0375ac62'
     weather_end_point = 'api.openweathermap.org/data/2.5/weather'
